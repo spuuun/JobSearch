864503bafd4cdcffc7ccd06cca45f4e58d5a6e6a
Matthew Ross <matthew.douglas.ross@gmail.com>
2019-12-28 23:36:14 -0600

    initial commit

---
+++ JobSearch/Models/ProspectiveJob.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobSearch.Models
+{
+    public class ProspectiveJob
+    {
+        public int JobId { get; set; }
+        public string CompanyName { get; set; }
+        public string PositionTitle { get; set; }
+        public DateTime AppliedOnDate { get; set; }
+    }
+}
